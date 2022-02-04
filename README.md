802a6a329ca0c1a5747707a7dcc0a82298f7d35e
niknal357 <46793548+niknal357@users.noreply.github.com>
2022-02-04 19:54:20 +0100

    .

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # uwufyer
+## what this project is about
+This project is a simple chrome extension that converts all text on webpages into uwu.
+## changelog
+TBA
--- content.js
+++ content.js
@@ -11,14 +11,34 @@ function uwuafy(text) {
             replacedText += 'W';
         } else if (currentChar == 'l' || currentChar == 'r' || currentChar == 'v') {
             replacedText += 'w';
-        } else if (currentChar == 'O' || currentChar == 'o') {
-            if (previousChar == 'N' || previousChar == 'n' || previousChar == 'M' || previousChar == 'm') {
+        } else if (currentChar == 'O') {
+            if (previousChar == 'N' || previousChar == 'M') {
+                replacedText += 'YO';
+            } else if (previousChar == 'n' || previousChar == 'm') {
+                replacedText += 'yO';
+            } else {
+                replacedText += currentChar;
+            }
+        } else if (currentChar == 'o') {
+            if (previousChar == 'N' || previousChar == 'M') {
+                replacedText += 'yo';
+            } else if (previousChar == 'n' || previousChar == 'm') {
                 replacedText += 'yo';
             } else {
                 replacedText += currentChar;
             }
-        } else if (currentChar == 'A' || currentChar == 'a') {
-            if (previousChar == 'N' || previousChar == 'n' || previousChar == 'M' || previousChar == 'm') {
+        } else if (currentChar == 'A') {
+            if (previousChar == 'N' || previousChar == 'M') {
+                replacedText += 'YA';
+            } else if (previousChar == 'n' || previousChar == 'm') {
+                replacedText += 'yA';
+            } else {
+                replacedText += currentChar;
+            }
+        } else if (currentChar == 'a') {
+            if (previousChar == 'N' || previousChar == 'M') {
+                replacedText += 'ya';
+            } else if (previousChar == 'n' || previousChar == 'm') {
                 replacedText += 'ya';
             } else {
                 replacedText += currentChar;
@@ -26,20 +46,17 @@ function uwuafy(text) {
         } else if (currentChar == '.') {
             a = 0;
         } else if (currentChar == ' ' && previousChar == '.') {
-            replacedText += ' ' + uwus[Math.floor(Math.random() * uwus.length)] + '. ';
+            replacedText += ' ' + uwus[Math.floor(Math.random() * uwus.length)] + '. ';
         } else if (currentChar != ' ' && previousChar == '.') {
-            replacedText += '. ';
+            replacedText += '.' + currentChar;
         } else {
             replacedText += currentChar;
         }
     }
-    if (previousChar == '.') {
-        replacedText += '.';
-    }
     return replacedText;
 }
 
-const ignore = ["www.google.com", "google.com", "www.youtube.com", "youtube.com"];
+const ignore = [];
 var passing = false;
 chrome.storage.sync.get(["toggled"], function(res) {
     var passing = res.toggled;
@@ -52,63 +69,16 @@ chrome.storage.sync.get(["toggled"], function(res) {
 
             for (var i = 0; i < elements.length; i++) {
                 var element = elements[i];
-                //setTimeout(() => {
+                if (element.tagName == "STYLE" || element.tagName == "SCRIPT" || element.tagName == "DIV") {
+                    continue;
+                }
                 for (var j = 0; j < element.childNodes.length; j++) {
                     var node = element.childNodes[j];
                     if (node.nodeType === 3) {
                         var text = node.nodeValue;
-                        var replacedText = text;
-                        replacedText = replacedText.replace(/r/, 'w');
-                        replacedText = replacedText.replace(/R/, 'W');
-                        replacedText = replacedText.replace(/l/, 'w');
-                        replacedText = replacedText.replace(/L/, 'W');
-                        replacedText = replacedText.replace(/v/, 'w');
-                        replacedText = replacedText.replace(/V/, 'W');
-                        replacedText = replacedText.replace(/no/, 'nyo');
-                        replacedText = replacedText.replace(/na/, 'nya');
-                        replacedText = replacedText.replace(/No/, 'Nyo');
-                        replacedText = replacedText.replace(/Na/, 'Nya');
-                        replacedText = replacedText.replace(/NO/, 'NYO');
-                        replacedText = replacedText.replace(/NA/, 'NYA');
-                        replacedText = replacedText.replace(/mo/, 'myo');
-                        replacedText = replacedText.replace(/ma/, 'mya');
-                        replacedText = replacedText.replace(/Mo/, 'Myo');
-                        replacedText = replacedText.replace(/Ma/, 'Mya');
-                        replacedText = replacedText.replace(/MO/, 'MYO');
-                        replacedText = replacedText.replace(/MA/, 'MYA');
-                        replacedText = replacedText.replace(/q\./, 'q uwu.');
-                        replacedText = replacedText.replace(/w\./, 'w uwu.');
-                        replacedText = replacedText.replace(/e\./, 'e uwu.');
-                        replacedText = replacedText.replace(/r\./, 'r uwu.');
-                        replacedText = replacedText.replace(/t\./, 't uwu.');
-                        replacedText = replacedText.replace(/y\./, 'y uwu.');
-                        replacedText = replacedText.replace(/i\./, 'i uwu.');
-                        replacedText = replacedText.replace(/o\./, 'o uwu.');
-                        replacedText = replacedText.replace(/p\./, 'p uwu.');
-                        replacedText = replacedText.replace(/a\./, 'a uwu.');
-                        replacedText = replacedText.replace(/s\./, 's uwu.');
-                        replacedText = replacedText.replace(/d\./, 'd uwu.');
-                        replacedText = replacedText.replace(/f\./, 'f uwu.');
-                        replacedText = replacedText.replace(/g\./, 'g uwu.');
-                        replacedText = replacedText.replace(/h\./, 'h uwu.');
-                        replacedText = replacedText.replace(/j\./, 'j uwu.');
-                        replacedText = replacedText.replace(/k\./, 'k uwu.');
-                        replacedText = replacedText.replace(/l\./, 'l uwu.');
-                        replacedText = replacedText.replace(/z\./, 'z uwu.');
-                        replacedText = replacedText.replace(/x\./, 'x uwu.');
-                        replacedText = replacedText.replace(/c\./, 'c uwu.');
-                        replacedText = replacedText.replace(/v\./, 'v uwu.');
-                        replacedText = replacedText.replace(/b\./, 'b uwu.');
-                        replacedText = replacedText.replace(/n\./, 'n uwu.');
-                        replacedText = replacedText.replace(/m\./, 'm uwu.');
-
-                        //alert(text);
-                        if (text !== replacedText) {
-                            element.replaceChild(document.createTextNode(replacedText), node);
-                        }
+                        node.nodeValue = uwuafy(text);
                     }
                 }
-                //}, 1 * i)
             }
         }, 100)
     }
